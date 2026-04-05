24378907398f3384353564a357a7a3a6720ff577
salmn-tkyk <salman.hafidzuddin05@gmail.com>
2026-04-05 23:02:04 +0700

    menambahkan class KuliahMahasiswa103082400020

--- tpmodul7_HYMAP_4/Program.cs
+++ tpmodul7_HYMAP_4/Program.cs
@@ -4,5 +4,10 @@
     {
         DataMahasiswa103082400020 dataMahasiswa = new DataMahasiswa103082400020();
         dataMahasiswa.ReadJSON();
+
+        Console.WriteLine();
+
+        KuliahMahasiswa103082400020 kuliahMahasiswa = new KuliahMahasiswa103082400020();
+        kuliahMahasiswa.ReadJSON();
     }
 }
