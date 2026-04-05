2e9befe0f1aaef8f615290b63faa5a737dc01362
IGedePutuArthaJaya99 <arthawinangun05@gmail.com>
2026-04-05 21:51:52 +0700

    menambahkan class KuliahMahasiswa103082400036

--- tpmodul7_HYMAP_4/Program.cs
+++ tpmodul7_HYMAP_4/Program.cs
@@ -2,7 +2,12 @@
 {
     static void Main(string[] args)
     {
-        DataMahasiswa103082400036 dataSaya = new DataMahasiswa103082400036();
-        dataSaya.ReadJSON();
+        DataMahasiswa103082400036 dataMahasiswa = new DataMahasiswa103082400036();
+        dataMahasiswa.ReadJSON();
+
+        Console.WriteLine();
+
+        KuliahMahasiswa103082400036 kuliahMahasiswa = new KuliahMahasiswa103082400036();
+        kuliahMahasiswa.ReadJSON();
     }
 }
